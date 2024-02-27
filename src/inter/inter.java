7a9b4d7836e55e09f53ad04ccd0d4bc25db77f52
VIGNESH D <107434564+VIGNESH-D04@users.noreply.github.com>
2024-02-27 22:48:12 +0530

    Add code files

---
+++ src/inter/inter.java
@@ -0,0 +1,29 @@
+package inter;
+
+import bean.Charitybean;
+import bean.charity;
+import bean.charityrequest;
+import bean.fakecheck;
+import bean.transfer;
+
+import bean.publicreg;
+
+public interface inter {
+	
+	public int creg(Charitybean cb);
+	
+	public int preg(publicreg pb);
+	
+	public boolean plog(publicreg pl);
+	
+	public int creq(charityrequest cr);
+	
+	public int ches(charity ck);
+	
+	public boolean clog(Charitybean cl);
+	
+	public int trans(transfer tr);
+	
+	
+
+}
